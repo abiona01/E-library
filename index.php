24d72ba5278c8007b4911a533f917dee4f91e56c
abiona01 <quadratwemimo@gmail.com>
2020-12-15 14:40:40 +0100

    php conection establishment

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 include('include/header.php');
 include('include/config.php');
 
+//get book table data from db;
+$select_query = "SELECT * FROM book ORDER by date";
+$result = mysqli_query($conn, $select_query);
+$book = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//print_r($food_details);
+
+//end of connection
+mysqli_close($conn); 
+
+?>
 ?>
 
 
