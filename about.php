d86cf197676e8b216ae51b2797bb5982ca4881ef
Ayomide <ayomideolaniran@gmail.com>
2020-12-21 13:16:27 +0100

    added about page

---
+++ about.php
@@ -0,0 +1,86 @@
+<?php
+
+    include('include/header.php');
+    include('include/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <div class="container">
+    <h4 class="container center-align">About Us</h4>
+    <p>Welcome to Sparky Library. This is an online Library where all books can be found. We are here for your reading pleasure... Enjoy your read
+    This idea was brought together by 6 web developers and we put in our best to bring this idea to light</p>
+    <div class="row">
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="./image/4.jpg">
+        </div>
+        <div class="card-content">
+          <p>Brief description and the role played</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="./image/4.jpg">
+        </div>
+        <div class="card-content">
+          <p>Brief description and the role played</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="./image/4.jpg">
+        </div>
+        <div class="card-content">
+          <p>Brief description and the role played</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <div class="row">
+  <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="./image/4.jpg">
+        </div>
+        <div class="card-content">
+          <p>Brief description and the role played</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="./image/4.jpg">
+        </div>
+        <div class="card-content">
+          <p>Brief description and the role played</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="./image/4.jpg">
+        </div>
+        <div class="card-content">
+          <p>Brief description and the role played</p>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
+
+<?php
+include('include/footer.php')
+?>
