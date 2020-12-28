62c6c3a0c445d00902e7d03f3fa79981fc8eef2a
abiona01 <quadratwemimo@gmail.com>
2020-12-28 08:59:51 +0100

    update on header

--- include/header.php
+++ include/header.php
@@ -56,14 +56,14 @@
           <ul class="right hide-on-med-and-down">
             <li><a href="home.php">Home</a></li>
             <li><a href="all.php">All Books</a></li>
-            <li><a href="about.php">About Us</a></li>
+            <li><a href="about.php">About</a></li>
             <li><a href="signup.php">Sign Up</a></li>
             <li><a href="box.php">Chat</a></li>
           </ul>
           <ul class="sidenav white" id="mobile-menu">
             <li><a href="index.php">Home</a></li>
             <li><a href="all.php">All Books</a></li>
-            <li><a href="about.php">About Us</a></li>
+            <li><a href="about.php">About</a></li>
             <li><a href="signup.php">Sign Up</a></li>
             <li><a href="box.php">Chat</a></li>
           </ul>
